23b878de318b459d66d72bc86cd65b3cb1c723fa
script000kiddies000 <scriptkiddies@protonmail.com>
2021-06-07 13:29:54 +0700

    dom complete + fix button text

--- js/dom.js
+++ js/dom.js
@@ -1,27 +1,43 @@
 const UNCOMPLETED_LIST_BOOK_ID = "incompleteBookshelfList";
 const COMPLETED_LIST_BOOK_ID = "completeBookshelfList"; 
 
+const checkbox = document.getElementById("inputBookIsComplete");
+checkbox.addEventListener("click", function(){
+    button = document.querySelector("#bookSubmit span");
+    if(checkbox.checked){
+        button.innerText = "selesai dibaca";
+    }else{
+        button.innerText = "Belum selesai dibaca";
+    }
+    
+});
+
 function addTodo() {
     const uncompletedBOOKList = document.getElementById(UNCOMPLETED_LIST_BOOK_ID );
-
+    const listCompleted = document.getElementById(COMPLETED_LIST_BOOK_ID);
 
     const BookTitle = document.getElementById("inputBookTitle").value;
     const BookAuthor = document.getElementById("inputBookAuthor").value;
     const BookYear = document.getElementById("inputBookYear").value;
     const BookIsComplete = document.getElementById("inputBookIsComplete").checked;
 
-    // console.log("book title " + BookTitle);
-    // console.log("book author " + BookAuthor);
-    // console.log("book year " + BookYear);
-    // console.log("is complete " + BookIsComplete);
+    console.log("book title " + BookTitle);
+    console.log("book author " + BookAuthor);
+    console.log("book year " + BookYear);
+    console.log("is complete " + BookIsComplete);
 
-    const book = makeTodo(BookTitle, BookAuthor, BookYear);
-    uncompletedBOOKList.append(book);
+    if(BookIsComplete){
+        const book = makeTodo(BookTitle, BookAuthor, BookYear, BookIsComplete);
+        listCompleted.append(book);
+    }else{
+        const book = makeTodo(BookTitle, BookAuthor, BookYear, BookIsComplete);
+        uncompletedBOOKList.append(book);
+    }
 
 
 }
 
-function makeTodo(title, author, year) {
+function makeTodo(title, author, year, isComplete) {
      
     const textTitle = document.createElement("h3");
     textTitle.innerText = title;
@@ -42,7 +58,11 @@ function makeTodo(title, author, year) {
     containerAction.classList.add('action');
 
     //bikin tombol selesai dan masukkan ke container action
-    containerAction.append(createCheckButton());
+    if(isComplete){
+        containerAction.append(createUndoButton(), createTrashButton());
+    }else{
+        containerAction.append(createCheckButton(), createTrashButton());
+    }
 
     //masukkan container action ke container book_item
     container.append(containerAction);
@@ -50,11 +70,25 @@ function makeTodo(title, author, year) {
     return container;
 }
 
+//list create button
 function createCheckButton() {
     return createButton("Selesai dibaca","green", function(event){
         addBookToCompleted(event.target.parentElement.parentElement);
     });
 }
+
+function createTrashButton() {
+    return createButton("Hapus buku","red", function(event){
+        removeBookFromUncompleted(event.target.parentElement.parentElement);
+    });
+}
+
+function createUndoButton() {
+    return createButton("Belum selesai di Baca","green", function(event){
+        undoBookFromCompleted(event.target.parentElement.parentElement);
+    });
+}
+//endlist create button
 
 
 // universal function
@@ -75,9 +109,27 @@ function addBookToCompleted(bookElement) {
     const BookIsComplete = true;
 
  
-    const newBook = makeTodo(BookTitle, BookAuthor, BookYear);
+    const newBook = makeTodo(BookTitle, BookAuthor, BookYear, BookIsComplete);
 
     const listCompleted = document.getElementById(COMPLETED_LIST_BOOK_ID);
     listCompleted.append(newBook);
     bookElement.remove();
 } 
+
+
+function removeBookFromUncompleted(bookElement) {
+    bookElement.remove();
+}
+
+function undoBookFromCompleted(bookElement){
+    const BookTitle = bookElement.querySelector(".book_item > h3").innerText;
+    const BookAuthor = bookElement.querySelectorAll(".book_item > p")[0].innerText;
+    const BookYear = bookElement.querySelectorAll(".book_item > p")[1].innerText;
+    const BookIsComplete = false;
+ 
+    const newBook = makeTodo(BookTitle, BookAuthor, BookYear, BookIsComplete);
+ 
+    const listUncompleted = document.getElementById(UNCOMPLETED_LIST_BOOK_ID);
+    listUncompleted.append(newBook);
+    bookElement.remove();
+}
