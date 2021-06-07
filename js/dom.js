56f21cd53ff6042fba835b2d15dcfc11dfa8e0cf
script000kiddies000 <scriptkiddies@protonmail.com>
2021-06-07 10:06:46 +0700

    tambah function makeTodo()

--- js/dom.js
+++ js/dom.js
@@ -1,4 +1,9 @@
+const UNCOMPLETED_LIST_BOOK_ID = "incompleteBookshelfList";
+
 function addTodo() {
+    const uncompletedBOOKList = document.getElementById(UNCOMPLETED_LIST_BOOK_ID );
+
+
     const BookTitle = document.getElementById("inputBookTitle").value;
     const BookAuthor = document.getElementById("inputBookAuthor").value;
     const BookYear = document.getElementById("inputBookYear").value;
@@ -9,4 +14,26 @@ function addTodo() {
     console.log("book year " + BookYear);
     console.log("is complete " + BookIsComplete);
 
+    const book = makeTodo(BookTitle, BookAuthor, BookYear);
+    uncompletedBOOKList.append(book);
+
+
+}
+
+function makeTodo(title, author, year) {
+     
+    const textTitle = document.createElement("h3");
+    textTitle.innerText = title;
+ 
+    const textAuthor = document.createElement("p");
+    textAuthor.innerText = author;
+
+    const textYear = document.createElement("p");
+    textYear.innerText = year;
+ 
+    const container = document.createElement("div");
+    container.classList.add("book_item")
+    container.append(textTitle, textAuthor, textYear);
+ 
+    return container;
 }
